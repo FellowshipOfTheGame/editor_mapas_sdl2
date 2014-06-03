39c9508907c2f9bd63a2f07cdcf3bb2876c5e5b9
lcsvcn <lcs.vcn@gmail.com>
2014-06-02 22:28:03 -0300

    Stable version -- with undo and redo not implement totally

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/* 
+* CREATED BY Lucas Vieira Costa Nicolau <-> FROM USP University
+* Used amoung with SDL2 
+* -- (good tutorial tip : SDL LAZY FOO --there's other, but this was the most 
+* used by me on learning)
+*
+* -- Open Source, Open to learn, Open to distribute. 
+* Just don't misrepresent my credits and time spend to make this library. Thanks.
+*/
 #include "editor_mapa.h"
 
 int main() {
---
+++ main.cpp~
@@ -0,0 +1,11 @@
+#include "editor_mapa.h"
+
+int main() {
+    int num = 0;
+    if (!editor_mapa()) {
+        fprintf(stderr, "Error on execution\n");
+        num = 1;
+    }
+    return num;
+}
+
